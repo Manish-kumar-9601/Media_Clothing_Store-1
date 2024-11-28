4a502903bb5fe158b2376ce71b1a6b56b93a537b
kris <chriscolin344@gmail.com>
2024-11-28 23:17:05 +0530

    some small changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Navbar from "./Navbar";
 import Bestseller from "./Bestseller";
 import Testimonies from "./Testimonies";
 import HomePage from "./HomePage";
+import ClothInfo from "./ClothInfo";
 // import Tempdata from "./assets/tempData.json"
 //Libraries
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route  } from "react-router-dom";
 import ErrorPage from "./ErrorPage";
 //Stylesheet
 import "./App.css";
@@ -18,6 +19,8 @@ function App() {
         <Route exact index path="/" element={<HomePage />}></Route>
         <Route path="/home" element={<HomePage />}></Route>
         <Route path="/bestseller" element={<Bestseller />}></Route>
+        <Route path="/clothinfo"  element={<ClothInfo />}></Route>
+        <Route path="/clothinfo/:id"  element={<ClothInfo />}></Route>
         <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
     </>
